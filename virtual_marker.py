ec00d311df487d86e15e6a06dd335e8890797616
ARUNAGIRINATHAN_K <167340745+arun-techverse@users.noreply.github.com>
2025-07-13 05:39:17 +0000

    Webcam & canvas setup

--- virtual_marker.py
+++ virtual_marker.py
@@ -6,3 +6,5 @@ import numpy as np
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
+
+# Webcam & canvas setup
